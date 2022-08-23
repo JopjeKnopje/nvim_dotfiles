beb8c1bc4b86068a46ee2412b638c62dcd550765
Joppe Boeve <jboeve@f0r3s12.codam.nl>
2022-08-23 20:10:35 +0200

    jup

--- lua/user/keymappings.lua
+++ lua/user/keymappings.lua
@@ -1,2 +1,2 @@
 vim.g.mapleader = "-"
-vim.api.nvim_set_keymap("", "<leader>", ":w | :!../compile.sh % <CR>", {noremap = true})
+vim.api.nvim_set_keymap("", "<leader>", ":w | :!./compile.sh % <CR>", {noremap = true})
