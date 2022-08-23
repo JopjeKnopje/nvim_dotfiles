41204b4a70266321e786411753e4340954c24dee
Joppe Boeve <jboeve@f0r4s14.codam.nl>
2022-08-23 15:54:53 +0200

    tried 42header

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,8 +56,8 @@ return packer.startup(function(use)
 		  'nvim-lualine/lualine.nvim',
 		    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 	    }
-	
 
+	
 	-- color themes
     	use	'morhetz/gruvbox'
 	use 	'LunarVim/darkplus.nvim'
